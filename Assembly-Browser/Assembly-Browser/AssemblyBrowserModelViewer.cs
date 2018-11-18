c416e228eb784987a0752f629af0c128bc58fd63
Sergei <s.bysov@mail.ru>
2018-11-19 01:15:36 +0300

    Add Command and Assembly Reader

--- Assembly-Browser/Assembly-Browser/AssemblyBrowserModelViewer.cs
+++ Assembly-Browser/Assembly-Browser/AssemblyBrowserModelViewer.cs
@@ -11,6 +11,38 @@ namespace Assembly_Browser
 {
     public class AssemblyBrowserModelViewer : INotifyPropertyChanged
     {
+        AssemblyReader assemblyReader;
 
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected void LoadAssembly(object o)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Filter = "Assemblies|*.dll;*.exe",
+                Title = "Select assembly",
+                Multiselect = false
+            };
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                assemblyReader = new AssemblyReader(openFileDialog.FileName);
+                OnPropertyChanged();
+            }
+        }
+
+        public Command LoadCommand
+        { get; protected set; }
+
+        public AssemblyBrowserModelViewer()
+        {
+            assemblyReader = null;
+            LoadCommand = new Command(LoadAssembly);
+        }
     }
 }
---
+++ Assembly-Browser/Assembly-Browser/Command.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Assembly_Browser
+{
+    public class Command : ICommand
+    {
+        private Action<object> execute;
+        private Func<object, bool> canExecute;
+
+        public Command(Action<object> execute, Func<object, bool> canExecute = null)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return canExecute == null || canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            execute(parameter);
+        }
+
+        public event EventHandler CanExecuteChanged;
+    }
+}
