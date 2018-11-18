3d76c6f895cdb8c7d5d8b8fee14428572291e116
Sergei <s.bysov@mail.ru>
2018-11-18 16:30:02 +0300

    Add Aswembly Viewer

---
+++ Assembly-Browser/Assembly-Browser/AssemblyBrowserModelViewer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Forms;
+
+namespace Assembly_Browser
+{
+    public class AssemblyBrowserModelViewer : INotifyPropertyChanged
+    {
+
+    }
+}
